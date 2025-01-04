07301c9d8186ae3f2cc05cdcd91d17946c4036b3
ycj <1870884413@qq.com>
2025-01-04 12:04:21 +0800

    更新笔记仓库主页

--- docs/.vitepress/views/Note/index.vue
+++ docs/.vitepress/views/Note/index.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        笔记Notice
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-5xl font-bold">小八的笔记仓库</h1>
+        <div class="mt-4">还在建设中....</div>
+        <div class="mt-4">欢迎大家帮助修正，完善笔记的内容</div>
+        <div class="mt-4">文章最下方有修改按钮，可以直接编辑文章</div>
     </div>
 </template>
 
--- docs/src/note/index.md
+++ docs/src/note/index.md
@@ -1,10 +1,7 @@
 ---
 layout: page
 ---
-# 笔记主页
 
-这里是笔记主页，欢迎来到我的笔记。
-还在完善中，欢迎给出建议
 <script setup>
 import Note from '../../../docs/.vitepress/views/Note/index.vue'
 </script>
